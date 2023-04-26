913ecd9f2e263ea41aa5456752aa8c6f8118e955
Rhys Saldanha <rhys-saldanha@gmail.com>
2023-04-26 22:23:17 +0100

    Add Reciplease icons, remove generated content and styling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="apple-touch-icon" href="/logo192.png"/>
+        {/*manifest.json provides metadata used when your web app is installed on a*/}
+        {/*user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/*/}
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="icon" href="/reciplease-book.svg"/>
+      </Head>
       <body>
       <Main/>
       <NextScript/>
